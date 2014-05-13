815f8d1c7490e4424b0701977b7ff3f442c5dd65
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-05-13 14:12:10 +0200

    also include empty values

--- src/test/java/de/avgl/dmp/graph/json/test/LiteralNodeTest.java
+++ src/test/java/de/avgl/dmp/graph/json/test/LiteralNodeTest.java
@@ -23,6 +23,17 @@ public class LiteralNodeTest {
 	}
 
 	@Test
+	public void testSerializeEmptyLiteralNode() throws IOException {
+
+		final LiteralNode literalNode = new LiteralNode(267, "");
+		final String literalNodeJSONString = Util.getJSONObjectMapper().writeValueAsString(literalNode);
+
+		final String expectedJSONString = TestUtil.getResourceAsString("empty_literal_node.json");
+
+		Assert.assertEquals("wrong serialisation", expectedJSONString, literalNodeJSONString);
+	}
+
+	@Test
 	public void testDeserializeLiteralNode() throws IOException {
 
 		final String literalNodeJSONString = TestUtil.getResourceAsString("literal_node.json");
@@ -31,6 +42,20 @@ public class LiteralNodeTest {
 		Assert.assertNotNull("deserialized literal node shouldn't be null", literalNode);
 
 		final LiteralNode expectedLiteralNode = new LiteralNode(267, "907");
+
+		Assert.assertEquals("ids of the literal nodes should be equal", expectedLiteralNode.getId(), literalNode.getId());
+		Assert.assertEquals("values of the literal nodes should be equal", expectedLiteralNode.getValue(), literalNode.getValue());
+	}
+
+	@Test
+	public void testDeserializeEmptyLiteralNode() throws IOException {
+
+		final String literalNodeJSONString = TestUtil.getResourceAsString("empty_literal_node.json");
+		final LiteralNode literalNode = Util.getJSONObjectMapper().readValue(literalNodeJSONString, LiteralNode.class);
+
+		Assert.assertNotNull("deserialized literal node shouldn't be null", literalNode);
+
+		final LiteralNode expectedLiteralNode = new LiteralNode(267, "");
 
 		Assert.assertEquals("ids of the literal nodes should be equal", expectedLiteralNode.getId(), literalNode.getId());
 		Assert.assertEquals("values of the literal nodes should be equal", expectedLiteralNode.getValue(), literalNode.getValue());
