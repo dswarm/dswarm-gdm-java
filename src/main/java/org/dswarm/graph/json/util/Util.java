7f2f64e30bd13629fd26826d52572bf896c1b753
Thomas Gängler <thomas.gaengler@slub-dresden.de>
2016-10-04 12:53:22 +0200

    [DD-1319] (re-)added GDM simple JSON serialisation (incl. tests)

--- src/main/java/org/dswarm/graph/json/util/Util.java
+++ src/main/java/org/dswarm/graph/json/util/Util.java
@@ -38,7 +38,9 @@ import org.dswarm.graph.json.Resource;
 import org.dswarm.graph.json.ResourceNode;
 import org.dswarm.graph.json.Statement;
 import org.dswarm.graph.json.util.helper.ConverterHelper;
+import org.dswarm.graph.json.util.helper.ConverterHelper2;
 import org.dswarm.graph.json.util.helper.ConverterHelperGDMHelper;
+import org.dswarm.graph.json.util.helper.ConverterHelperGDMHelper2;
 
 /**
  * @author tgaengler
@@ -89,7 +91,8 @@ public class Util {
 		return FACTORY;
 	}
 
-	public static JsonNode toGDMCompactJSON(final Model model, final Set<String> recordURIs) {
+	public static JsonNode toGDMCompactJSON(final Model model,
+	                                        final Set<String> recordURIs) {
 
 		if (model == null) {
 
@@ -158,7 +161,10 @@ public class Util {
 		return jsonArray;
 	}
 
-	public static JsonNode convertToGDMCompactJSON(final Resource recordResource, final Node resourceNode, final ArrayNode json, final Model model) {
+	public static JsonNode convertToGDMCompactJSON(final Resource recordResource,
+	                                               final Node resourceNode,
+	                                               final ArrayNode json,
+	                                               final Model model) {
 
 		final Map<String, ConverterHelper> converterHelpers = new LinkedHashMap<>();
 
@@ -236,6 +242,141 @@ public class Util {
 		return json;
 	}
 
+	public static JsonNode toGDMSimpleJSON(final Model model, final Set<String> recordURIs) {
+
+		if (model == null) {
+
+			LOG.debug("model is null, can't convert model to JSON");
+
+			return null;
+		}
+
+		if (recordURIs == null) {
+
+			LOG.debug("resource URI is null, can't convert model to JSON");
+
+			return null;
+		}
+
+		final Iterator<String> iter = recordURIs.iterator();
+
+		if (!iter.hasNext()) {
+
+			// no entries
+
+			return null;
+		}
+
+		final ArrayNode jsonArray = Util.getJSONObjectMapper().createArrayNode();
+
+		while (iter.hasNext()) {
+
+			final String resourceURI = iter.next();
+			final Resource recordResource = model.getResource(resourceURI);
+
+			if (recordResource == null) {
+
+				LOG.debug("couldn't find record resource for record  uri '{}' in model", resourceURI);
+
+				return null;
+			}
+
+			final ObjectNode json = Util.getJSONObjectMapper().createObjectNode();
+
+			// determine record resource node from statements of the record resource
+			final ResourceNode recordResourceNode = Util.getResourceNode(resourceURI, recordResource);
+
+			if (recordResourceNode == null) {
+
+				LOG.debug("couldn't find record resource node for record  uri '{}' in model", resourceURI);
+
+				return null;
+			}
+
+			convertToGDMSimpleJSON(recordResource, recordResourceNode, json, json);
+
+			if (json == null) {
+
+				// TODO: maybe log something here
+
+				continue;
+			}
+
+			final ObjectNode resourceJson = Util.getJSONObjectMapper().createObjectNode();
+
+			resourceJson.set(resourceURI, json);
+			jsonArray.add(resourceJson);
+		}
+
+		return jsonArray;
+	}
+
+	private static JsonNode convertToGDMSimpleJSON(final Resource recordResource,
+	                                               final Node resourceNode,
+	                                               final ObjectNode rootJson,
+	                                               final ObjectNode json) {
+
+		final Map<String, ConverterHelper2> converterHelpers = new LinkedHashMap<>();
+
+		// filter record resource statements to statements for subject uri/id (resource node))
+		final Set<Statement> statements = Util.getResourceStatement(resourceNode, recordResource);
+
+		for (final Statement statement : statements) {
+
+			final String propertyURI = statement.getPredicate().getUri();
+			final Node gdmNode = statement.getObject();
+
+			if (gdmNode instanceof LiteralNode) {
+
+				ConverterHelperGDMHelper2.addLiteralToConverterHelper(converterHelpers, propertyURI, gdmNode);
+
+				continue;
+			}
+
+			if (gdmNode instanceof ResourceNode) {
+
+				final ResourceNode object = (ResourceNode) gdmNode;
+
+				// filter record resource statements to statements for object uri (object node))
+				final Set<Statement> objectStatements = Util.getResourceStatement(object, recordResource);
+
+				if (objectStatements == null || objectStatements.isEmpty()) {
+
+					ConverterHelperGDMHelper2.addURIResourceToConverterHelper(converterHelpers, propertyURI, gdmNode);
+
+					continue;
+				}
+
+				// resource has an uri, but is deeper in the hierarchy -> it will be attached to the root json node as separate
+				// entry
+
+				final ObjectNode objectNode = Util.getJSONObjectMapper().createObjectNode();
+
+				final JsonNode jsonNode = convertToGDMSimpleJSON(recordResource, object, rootJson, objectNode);
+
+				rootJson.set(object.getUri(), jsonNode);
+
+				continue;
+			}
+
+			// node is (/must be) a blank node
+
+			final ObjectNode objectNode = Util.getJSONObjectMapper().createObjectNode();
+
+			final JsonNode jsonNode = convertToGDMSimpleJSON(recordResource, gdmNode, rootJson, objectNode);
+
+			ConverterHelperGDMHelper2.addJSONNodeToConverterHelper(converterHelpers, propertyURI, jsonNode);
+		}
+
+		for (final Map.Entry<String, ConverterHelper2> converterHelperEntry : converterHelpers.entrySet()) {
+
+			converterHelperEntry.getValue().build(json);
+		}
+
+		return json;
+	}
+
+
 	/**
 	 * Gets resource node for the given resource identifier in the given record resource.
 	 *
@@ -243,7 +384,8 @@ public class Util {
 	 * @param recordResource the GDM record resource
 	 * @return
 	 */
-	public static ResourceNode getResourceNode(final String resourceURI, final Resource recordResource) {
+	public static ResourceNode getResourceNode(final String resourceURI,
+	                                           final Resource recordResource) {
 
 		if (resourceURI == null || recordResource == null) {
 
@@ -299,7 +441,8 @@ public class Util {
 	 * @param recordResource the GDM record Resource
 	 * @return
 	 */
-	public static Set<Statement> getResourceStatement(final Node resourceNode, final Resource recordResource) {
+	public static Set<Statement> getResourceStatement(final Node resourceNode,
+	                                                  final Resource recordResource) {
 
 		if (resourceNode == null || recordResource == null) {
 
@@ -328,7 +471,8 @@ public class Util {
 	 * @param recordResource the GDM recourd resource
 	 * @return
 	 */
-	public static Set<Statement> getResourceStatement(final String resourceURI, final Resource recordResource) {
+	public static Set<Statement> getResourceStatement(final String resourceURI,
+	                                                  final Resource recordResource) {
 
 		if (resourceURI == null || recordResource == null) {
 
@@ -381,7 +525,8 @@ public class Util {
 	 * @param recordResource the GDM record resource
 	 * @return
 	 */
-	public static Set<Statement> getResourceStatement(final Long resourceId, final Resource recordResource) {
+	public static Set<Statement> getResourceStatement(final Long resourceId,
+	                                                  final Resource recordResource) {
 
 		if (resourceId == null || recordResource == null) {
 
